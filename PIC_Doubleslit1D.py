f3e14024f2682881c6d0a29fb554fbb6c6842c95
takepiyo <exmcube@gmail.comsource>
2022-08-22 18:16:24 +0900

    some arrangement

--- PIC_Doubleslit1D.py
+++ PIC_Doubleslit1D.py
@@ -1,43 +1,46 @@
+import math
+import argparse
+
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-
 from tqdm import tqdm
 
 
-class DoubleSlit1DSOC:
-    def __init__(self, tf, slit_t, dt, v, R, alpha, slit1, slit2, x_min, x_max) -> None:
-        self.tf = tf
-        self.slit_t = slit_t
-        self.dt = dt
-        self.v = v
-        self.R = R
-        self.alpha = alpha
+class DoubleSlit1DAnalytical:
+    def __init__(self, env):
+        self.env = env
+        self.tf = env.T
+        self.slit_t = env.slit_t
+        self.dt = env.dt
+        self.v = env.v
+        self.R = env.R
+        self.alpha = env.alpha
 
-        self.slit1 = slit1
-        self.slit2 = slit2
-        self.x_min = x_min
-        self.x_max = x_max
+        self.slit1 = env.slit1
+        self.slit2 = env.slit2
+        self.x_min = env.x_min
+        self.x_max = env.x_max
 
-        self.sigma = lambda t: math.sqrt(v * (tf - t))
+        self.sigma = lambda t: math.sqrt(self.v * (self.tf - t))
         self.sigma_1 = lambda t: math.sqrt(self.sigma(
-            t) ** 2 * v * R / (alpha * self.sigma(t) ** 2 + v * R))
-        self.A = lambda t: 1 / (self.slit_t - t) + 1 / (R + tf - slit_t)
-        self.B = lambda x, t: x / (slit_t - t)
+            t) ** 2 * self.v * self.R / (self.alpha * self.sigma(t) ** 2 + self.v * self.R))
+        self.A = lambda t: 1 / (self.slit_t - t) + 1 / \
+            (self.R + self.tf - self.slit_t)
+        self.B = lambda x, t: x / (self.slit_t - t)
         self.F = lambda x_0, x, t: math.erf(
-            math.sqrt(self.A(t) / (2 * v)) * (x_0 - (self.B(x, t) / self.A(t))))
+            math.sqrt(self.A(t) / (2 * self.v)) * (x_0 - (self.B(x, t) / self.A(t))))
         self.P = lambda x, t: self.F(
-            slit1[1], x, t) - self.F(slit1[0], x, t) + self.F(slit2[1], x, t) - self.F(slit2[0], x, t)
-        self.J = lambda x, t: v * R * math.log(self.sigma(t) / self.sigma_1(t)) + 0.5 * (
-            self.sigma_1(t) * x / self.sigma(t)) ** 2 - v * R * math.log(0.5 * (self.P(x, t)) + 1e-5) if t < slit_t else v * R * math.log(self.sigma(t) / self.sigma_1(t)) + 0.5 * (
-            self.sigma_1(t) * x / self.sigma(t)) ** 2 * alpha
+            self.slit1[1], x, t) - self.F(self.slit1[0], x, t) + self.F(self.slit2[1], x, t) - self.F(self.slit2[0], x, t)
+        self.J = lambda x, t: self.v * self.R * math.log(self.sigma(t) / self.sigma_1(t)) + 0.5 * (
+            self.sigma_1(t) * x / self.sigma(t)) ** 2 - self.v * self.R * math.log(0.5 * (self.P(x, t)) + 1e-5) if t < self.slit_t else self.v * self.R * math.log(self.sigma(t) / self.sigma_1(t)) + 0.5 * (
+            self.sigma_1(t) * x / self.sigma(t)) ** 2 * self.alpha
         self.partial_xF = lambda x_0, x, t: 2 / math.sqrt(math.pi) * math.exp(-math.sqrt(
-            self.A(t) / (2 * v)) * (x_0 - self.B(x, t) / self.A(t)) ** 2)
-        self.partial_xP = lambda x, t: self.partial_xF(slit1[1], x, t) - self.partial_xF(
-            slit1[0], x, t) + self.partial_xF(slit2[1], x, t) - self.partial_xF(slit2[0], x, t)
+            self.A(t) / (2 * self.v)) * (x_0 - self.B(x, t) / self.A(t)) ** 2)
+        self.partial_xP = lambda x, t: self.partial_xF(self.slit1[1], x, t) - self.partial_xF(
+            self.slit1[0], x, t) + self.partial_xF(self.slit2[1], x, t) - self.partial_xF(self.slit2[0], x, t)
 
-        self.optimal_u = lambda x, t: - (v * x) / (R + tf - t) - (self.partial_xP(x, t) / self.P(x, t)) * (v / (
-            math.sqrt(2 * v * self.A(t)) * (slit_t - t))) if t < slit_t else - (alpha * x) / (R + alpha * (tf - t))
+        self.optimal_u = lambda x, t: - (self.v * x) / (self.R + self.tf - t) - (self.partial_xP(x, t) / self.P(x, t)) * (self.v / (
+            math.sqrt(2 * self.v * self.A(t)) * (self.slit_t - t))) if t < self.slit_t else - (self.alpha * x) / (self.R + self.alpha * (self.tf - t))
 
     def command(self, x, t):
         return self.optimal_u(x, t)
@@ -52,8 +55,8 @@ class DoubleSlit1DSOC:
         plt.show()
 
 
-class DoubleSlitPathIntegral:
-    def __init__(self, env, N) -> None:
+class DoubleSlit1DPathIntegralSampling:
+    def __init__(self, env, N):
         self.env = env
         self.N = N
         self.dt = env.dt
@@ -75,15 +78,13 @@ class DoubleSlitPathIntegral:
         # env.render_multiple_path(self.traj)
 
         self.psi = sum(self.sampled_cost) / N
-        a = 1
 
     def command(self, x, t):
-        # return np.sum(self.sampled_cost * (self.noise_history[:, int(t / self.dt)])) / (self.psi * self.N)
         return np.sum(self.sampled_cost * (self.noise_history[:, 1])) / (self.psi * self.N)
 
 
 class DoubleSlit1D:
-    def __init__(self) -> None:
+    def __init__(self):
         self.T = 2.0
         self.dt = 0.02
         self.slit_t = 1.0
@@ -158,12 +159,18 @@ class DoubleSlit1D:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Path Integral Control Example(Double Slit 1D Environment)")
+    parser.add_argument('--method', choices=[
+                        'Analytical', 'Sampling'], default='Analytical')
+    parser.add_argument('-n', default=1000)
+    parser.add_argument('--n_sampling', default=100000)
+    args = parser.parse_args()
     env = DoubleSlit1D()
-    # ctrl = DoubleSlit1DSOC(env.T, env.slit_t, env.dt, env.v, env.R,
-    #                        env.alpha, env.slit1, env.slit2, env.x_min, env.x_max)
-    ctrl = DoubleSlitPathIntegral(env, 100000)
+    ctrl = DoubleSlit1DAnalytical(
+        env) if args.method == 'Analytical' else DoubleSlit1DPathIntegralSampling(env, args.n_sampling)
     traj = []
-    for _ in tqdm(range(1000)):
+    for _ in tqdm(range(args.n)):
         x, t, done = env.reset(x_0=-1.0)
         while not done:
             u = ctrl.command(x, t)
